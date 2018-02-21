bcd3b0de15e99d54e198f649f0d49d4c90d40296
David Hudson <david-h-3110@hotmail.co.uk>
2018-02-21 14:28:53 +0000

    Update generation command. Update server to implement new proto

--- api/server.go
+++ api/server.go
@@ -2,16 +2,14 @@ package api
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/sirupsen/logrus"
 
 	"github.com/heedson/riotgear/proto"
 )
 
 // Fix when dep isn't shit
-//go:generate protoc -I../proto -I../thirdparty/googleapis/ --go_out=plugins=grpc:$GOPATH/src --grpc-gateway_out=logtostderr=true:$GOPATH/src --swagger_out=logtostderr=true:../proto/ ../proto/api.proto
+//go:generate protoc -I../proto -I../thirdparty/googleapis/ --go_out=plugins=grpc:$GOPATH/src --grpc-gateway_out=logtostderr=true:$GOPATH/src ../proto/api.proto
 
 type Server struct {
 	logger *logrus.Logger
@@ -23,7 +21,10 @@ func NewServer(logger *logrus.Logger) *Server {
 	}
 }
 
-func (s *Server) Echo(ctx context.Context, req *proto.EchoRequest) (*empty.Empty, error) {
-	fmt.Println(req.GetValue())
-	return nil, nil
+func (s *Server) Echo(ctx context.Context, req *proto.EchoMsg) (*proto.EchoMsg, error) {
+	s.logger.Infoln(req.GetValue())
+
+	return &proto.EchoMsg{
+		Value: req.GetValue(),
+	}, nil
 }
