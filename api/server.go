0f010862e4a27f12a90b5317c899f925a942df98
David Hudson <david.hudson@infosum.com>
2018-03-14 17:35:33 +0000

    Extract player ID logic as many other methods will end up requiring it for processing

--- api/server.go
+++ api/server.go
@@ -3,38 +3,31 @@ package api
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/url"
 	"regexp"
 	"strings"
 	"time"
 
-	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"github.com/heedson/riotgear/gear"
 	"github.com/heedson/riotgear/proto"
 )
 
-var httpStatusToGRPCStatus = map[int]codes.Code{
-	// 400 Bad Request - INTERNAL.
-	http.StatusBadRequest: codes.Internal,
-	// 401 Unauthorized  - UNAUTHENTICATED.
-	http.StatusUnauthorized: codes.Unauthenticated,
-	// 403 Forbidden - PERMISSION_DENIED.
-	http.StatusForbidden: codes.PermissionDenied,
-	// 404 Not Found - NOT_FOUND.
-	http.StatusNotFound: codes.NotFound,
-	// 429 Too Many Requests - UNAVAILABLE.
-	http.StatusTooManyRequests: codes.Unavailable,
-	// 502 Bad Gateway - UNAVAILABLE.
-	http.StatusBadGateway: codes.Unavailable,
-	// 503 Service Unavailable - UNAVAILABLE.
-	http.StatusServiceUnavailable: codes.Unavailable,
-	// 504 Gateway timeout - UNAVAILABLE.
-	http.StatusGatewayTimeout: codes.Unavailable,
+func getGRPCError(err error) error {
+	switch err.(type) {
+	case gear.ErrInternal:
+		return status.Error(codes.Internal, err.Error())
+	case gear.ErrInvalidArgument:
+		return status.Error(codes.InvalidArgument, err.Error())
+	case nil:
+		return nil
+	default:
+		return status.Error(codes.Unknown, err.Error())
+	}
 }
 
 type Server struct {
@@ -60,51 +53,55 @@ func NewServer(logger *logrus.Logger, regionToServerURL map[string]*url.URL, rio
 	}
 }
 
-func (s *Server) GetPlayerID(ctx context.Context, pbReq *proto.PlayerIDReq) (*proto.PlayerID, error) {
-	if ok := s.nameRegexp.Match([]byte(pbReq.GetPlayerName())); !ok {
-		return nil, status.Errorf(codes.InvalidArgument, "%q is not a valid player name", pbReq.GetPlayerName())
-	}
-
-	rel := &url.URL{Path: fmt.Sprintf("/lol/summoner/v3/summoners/by-name/%s", pbReq.GetPlayerName())}
-
-	serverURL, ok := s.regionToServerURL[strings.ToLower(pbReq.GetRegion())]
+func (s *Server) getPlayerID(ctx context.Context, regionName, playerName string) (int, error) {
+	serverURL, ok := s.regionToServerURL[strings.ToLower(regionName)]
 	if !ok {
-		return nil, status.Errorf(codes.InvalidArgument, "%q is not a valid server region", pbReq.GetRegion())
+		return 0, gear.Errorf(gear.InvalidArgument, "%q is not a valid region name", regionName)
 	}
+
+	if ok := s.nameRegexp.Match([]byte(playerName)); !ok {
+		return 0, gear.Errorf(gear.InvalidArgument, "%q is not a valid player name", playerName)
+	}
+
+	rel := &url.URL{Path: "/lol/summoner/v3/summoners/by-name/" + playerName}
 
 	u := serverURL.ResolveReference(rel)
 
 	req, err := http.NewRequest("GET", u.String(), nil)
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return 0, gear.Wrap(gear.Internal, err)
 	}
 	req.Header.Set("Accept", "application/json")
 	req.Header.Set("X-Riot-Token", s.riotAPIKey)
 
 	resp, err := s.httpClient.Do(req)
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return 0, gear.Wrap(gear.Internal, err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		grpcCode, ok := httpStatusToGRPCStatus[resp.StatusCode]
-		if !ok {
-			grpcCode = codes.Unknown
-		}
-
-		return nil, status.Error(grpcCode, resp.Status)
+		return 0, gear.Error(gear.Internal, resp.Status)
 	}
 
 	var data = make(map[string]interface{})
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
+		return 0, gear.Wrap(gear.Internal, err)
 	}
 
 	playerID := int(data["id"].(float64))
 
-	s.logger.Infof("%s's player ID: %d\n", data["name"], playerID)
+	s.logger.Infof("%s's player ID: %d", data["name"], playerID)
+
+	return playerID, nil
+}
+
+func (s *Server) GetPlayerID(ctx context.Context, pbReq *proto.PlayerIDReq) (*proto.PlayerID, error) {
+	playerID, err := s.getPlayerID(ctx, pbReq.GetRegionName(), pbReq.GetPlayerName())
+	if err != nil {
+		return nil, getGRPCError(err)
+	}
 
 	return &proto.PlayerID{
 		PlayerId: int64(playerID),
